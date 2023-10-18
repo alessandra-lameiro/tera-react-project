5a4d8765286c817caee853148f77f32c2e7db9f6
alessandra-lameiro <134969777+alessandra-lameiro@users.noreply.github.com>
2023-10-18 16:40:04 -0300

    Adicionatela inicial

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,17 +2,45 @@ import React from "react";
 import logo from "../../images/logo.svg";
 
 export default function Home() {
-  return (
+  const [users, setUsers] = React.useState([]);
+  const [currentUser, setCurrentUser] = React.useState("");
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    fetch("https://63cf09718a780ae6e6710dbe.mockapi.io/users")
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data);
+        setIsLoading(false);
+      });
+  }, []);
+
+  const handleUserChange = (e) => setCurrentUser(e.target.value);
+
+  const handleButtonClick = (e) => console.log(e);
+
+  return isLoading ? (
+    <h1>Loading...</h1>
+  ) : (
     <div className="home center">
       <div className="home__logo">
         <img src={logo} className="responsive" alt="" />
       </div>
-      <select className="home__select-users">
-        <option>User 1</option>
-        <option>User 2</option>
-        <option>User 3</option>
+      <select onChange={handleUserChange} className="home__select-users">
+        <option value="">Selecionar usuário</option>
+        {users
+          .sort((a, b) => a.fn.localeCompare(b.fn))
+          .map((user) => (
+            <option value={user.id} key={user.id}>
+              {user.fn} {user.ln}
+            </option>
+          ))}
       </select>
-      <button className="button-primary">Entrar</button>
+      {!!currentUser && (
+        <button onClick={handleButtonClick} className="button-primary">
+          Entrar
+        </button>
+      )}
     </div>
   );
 }
