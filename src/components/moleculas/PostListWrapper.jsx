a6ec1f648b4f253293291914b7c1f04560737555
alessandra-lameiro <134969777+alessandra-lameiro@users.noreply.github.com>
2023-10-19 16:07:07 -0300

    parte 2

---
+++ src/components/moleculas/PostListWrapper.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PostListItem from "./PostListItem";
+
+export default function PostListWrapper() {
+  return (
+    <div className="user-blog__posts">
+      <PostListItem />
+    </div>
+  );
+}
